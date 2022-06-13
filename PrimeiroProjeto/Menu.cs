99cb5f22efff9999408a6f581df638929a3ca7ab
elianehenri <elianehenriqueta@gmail.com>
2022-06-13 13:58:41 -0300

    Exercicio 06

---
+++ PrimeiroProjeto/Menu.cs
@@ -0,0 +1,12 @@
+﻿
+
+public  class Menu
+{
+    public static void Start()
+    {
+       // string Titulo = "Seja bem vindo(a) ao GeraEstoque 1.0";
+       // Console.WriteLine(Titulo);
+
+
+    }
+}
--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿namespace GeraEstoque;
 
-namespace GeraEstoque
+class Program
 {
-   class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Exercicio 05!");
-        }
+        
+        Console.SetCursorPosition(5, 2);
+        Console.WriteLine("Seja bem vindo(a) ao GeraEstoque 1.0");
+        Console.SetCursorPosition(5, 4);
+        Console.WriteLine("===================================");
+        Console.SetCursorPosition(5, 5);
+        Console.WriteLine("1 - Cadastrar Produto");
+        Console.SetCursorPosition(5, 6);
+        Console.WriteLine("2 - Editar Produto");
+        Console.SetCursorPosition(5,7);
+        Console.WriteLine("3 - Excluir Produto");
+        Console.SetCursorPosition(5, 8);
+        Console.WriteLine("0 - Sair");
+        Console.SetCursorPosition(5, 11);
+        Console.Write(" Digite a Opçao: ");
+        Console.ReadLine();
+
     }
+
 }
