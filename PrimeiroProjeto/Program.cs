083b2f13ca31d7660af36d889a185538e68b47a1
elianehenri <elianehenriqueta@gmail.com>
2022-06-12 21:53:46 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GeraEstoque
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercicio 05!");
+        }
+    }
+}
